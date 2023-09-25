00317b9245520cd6ef96e0419b40c475f18519ea
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-09-25 14:11:04 -0400

    return colocation fixed

--- classes/RangeNumbers.Class.php
+++ classes/RangeNumbers.Class.php
@@ -167,13 +167,12 @@ class RangeNumbers {
         sort($arrayNumbers);
 
         for($i = 0; $i < count($totalNumbers); $i++) {
-            if($totalNumbers[$i] == $arrayNumbers) {
+            if($totalNumbers[$i] != $arrayNumbers) {
                 $this -> arrayNumbers();
                 break;
-            } else {                
-                return $arrayNumbers;
             }
         }
+        return $arrayNumbers;
     }
 }
 
