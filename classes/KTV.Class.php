8efe2852ef6ec157b65ab55d930ecc505fad47ea
Magdiel1990 <magdielmagdiel1@gmail.com>
2023-10-13 19:18:25 -0400

    kino class half done

--- classes/KTV.Class.php
+++ classes/KTV.Class.php
@@ -3,29 +3,25 @@ require_once ("Lottery.Class.php");
 
 class KTVClass extends LotteryClass {
     //Filter 6
-    protected function subRange($balls, $conn) {            
-        $array = $this -> sumRange($balls, $conn);
-
-     //   $array = $this -> diffRangeLoop ($array, $balls, $conn);
+    protected function subRange($days, $balls, $conn) {            
+        $array = $this -> sumRange($days, $balls, $conn);
 
         return $array;
     }
     
-    protected function consecutiveOutArray ($balls, $conn){
-        return $this -> rangeProArray ($balls, $conn);    
+    protected function consecutiveOutArray ($days, $balls, $conn){
+        return $this -> rangeProArray ($days, $balls, $conn);    
     }
 
     //Filter 10
-    protected function sumEach($balls, $conn) {
-        $array = $this -> consecutiveOutArray($balls, $conn);
-        
-       // $array = $this -> sumEachLoop($array, $balls, $conn);
+    protected function sumEach($days, $balls, $conn){
+        $array = $this -> consecutiveOutArray($days, $balls, $conn);
    
         return $array;
     }
 
-    protected function insersectArrayOut ($amount, $up, $balls, $conn, $frequency) {
-        $array = $this -> randOutArray($amount, $balls, $up, $conn);
+    protected function insersectArrayOut ($days, $balls, $conn, $frequency) {
+        $array = $this -> sumEach($days, $balls, $conn);
         $totalPlays = $this -> totalPlays($conn);
 
         sort($array);
@@ -40,20 +36,20 @@ class KTVClass extends LotteryClass {
         $array = $this -> intersectCompare($array, 12, $balls, ceil(($totalPlays - 1) * $frequency), $totalPlays - 1, $conn);
         $array = $this -> intersectCompare($array, 11, $balls, ceil(($totalPlays - 1) * $frequency), $totalPlays - 1, $conn);
         $array = $this -> intersectCompare($array, 10, $balls, ceil(($totalPlays - 1) * $frequency), $totalPlays - 1, $conn);
-        $array = $this -> intersectCompare($array, 9, $balls, ceil(($totalPlays - 1) * $frequency), $totalPlays - 1, $conn);
-        $array = $this -> intersectCompare($array, 8, $balls, ceil(($totalPlays - 1) * $frequency), $totalPlays - 1, $conn);
         
         return $array;
     }  
 
-    protected function lastRange($amount, $balls, $up, $conn) {
-        $array = $this -> number_period_filter ($amount, $up, $balls, $conn, 0.02);
-        
-        for($i = 1; $i<=6; $i++) {
-            $array = $this -> range_filter($array, $i, 5*$i);
-        }
-        
-        $array = $this -> range_filter($array, 7, 30);
+    protected function lastRange($days, $balls, $conn, $frequency) {
+        $array = $this -> number_period_filter ($days, $balls, $conn, $frequency);
+
+        $array = $this -> range_filter($array, 1, 5);
+        $array = $this -> range_filter($array, 2, 10);
+        $array = $this -> range_filter($array, 3, 15);
+        $array = $this -> range_filter($array, 4, 20);
+        $array = $this -> range_filter($array, 5, 25);
+        $array = $this -> range_filter($array, 6, 30);
+        $array = $this -> range_filter($array, 7, 35);
         $array = $this -> range_filter($array, 8, 40);
         $array = $this -> range_filter($array, 9, 45);
         $array = $this -> range_filter($array, 10, 50);
@@ -70,8 +66,8 @@ class KTVClass extends LotteryClass {
         return $array;
     }
 
-    private function tenNumbersCreator($balls, $up, $conn) {
-        $array = $this -> lastRange(200000, $balls, $up, $conn);
+    private function tenNumbersCreator($days, $balls, $conn, $frequency) {
+        $array = $this -> lastRange($days, $balls, $conn, $frequency);
         //Alearoriamente organizamos array
         shuffle($array);
         //Lo divido en pedazos de 10
@@ -81,8 +77,8 @@ class KTVClass extends LotteryClass {
     }
 
 //Final
-    public function finalNumbers ($balls, $up, $conn) {
-        $array = $this -> tenNumbersCreator($balls, $up, $conn);
+    public function finalNumbers ($days, $balls, $conn, $frequency) {
+        $array = $this -> tenNumbersCreator($days, $balls, $conn, $frequency);
         sort($array);
         return $array;
     }
