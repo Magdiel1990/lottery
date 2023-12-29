bdd19ab41527bee3939aab98a412feef3a826273
Magdiel1990 <magdielmagdiel1@gmail.com>
2023-12-29 11:23:04 -0400

    style change

--- pages/index.view.php
+++ pages/index.view.php
@@ -2,28 +2,25 @@
 <?php
 require_once ("partials/head.php");
 require_once ("partials/nav.php");
+
+$titles = ["Loto Pool", "Loto", "Kino TV"];
+$links = [root. 'LP/agregar', root. 'Loto/agregar', root. 'Kino/agregar'];
 ?>
 
-    <main class="container p-4">
-        <div class="text-center mt-4 d-flex justify-content-around align-items-center"> 
-            <div class="card" style="width: 14rem;">
+    <main class="container p-4 my-4">
+        <div class="text-center d-flex flex-column flex-md-row align-items-center justify-content-md-around"> 
+        <?php
+            for($i = 0; $i < count($titles); $i++) {
+        ?>        
+            <div class="card mb-2 mx-md-2" style="min-width: 12rem;">
                 <div class="card-body">
-                    <h5 class="card-title text-success">Loto Pool</h5>
-                    <a href="<?php echo root. 'LP/agregar'?>" class="btn btn-primary my-3">Acceder</a>
+                    <h5 class="card-title text-success"><?php echo $titles[$i];?></h5>
+                    <a href="<?php echo $links[$i];?>" class="btn btn-primary my-3">Acceder</a>
                 </div>
             </div>
-            <div class="card" style="width: 14rem;">
-                <div class="card-body">
-                    <h5 class="card-title text-success">Loto</h5>
-                    <a href="<?php echo root. 'Loto/agregar'?>" class="btn btn-primary my-3">Acceder</a>
-                </div>
-            </div>
-            <div class="card" style="width: 14rem;">
-                <div class="card-body">
-                    <h5 class="card-title text-success">Kino TV</h5>
-                    <a href="<?php echo root. 'Kino/agregar'?>" class="btn btn-primary my-3">Acceder</a>
-                </div>
-            </div>
+        <?php
+            }
+        ?>
         </div>
     </main>
 <?php
