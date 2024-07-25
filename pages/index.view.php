ccfc6ab152b56abbe06416873b05e4fcc1a6d564
Magdiel1990 <magdielmagdiel1@gmail.com>
2024-07-24 22:03:45 -0400

    index page

--- pages/index.view.php
+++ pages/index.view.php
@@ -3,19 +3,17 @@
 require_once ("partials/head.php");
 require_once ("partials/nav.php");
 
-$titles = ["Loto Pool", "Loto", "Kino TV"];
-$links = [root. 'LP/agregar', root. 'Loto/agregar', root. 'Kino/agregar'];
+$titles = ["Loto" => root. 'Loto/agregar'];
 ?>
-
     <main class="container p-4 my-4">
         <div class="text-center d-flex flex-column flex-md-row align-items-center justify-content-md-around"> 
         <?php
-            for($i = 0; $i < count($titles); $i++) {
+            foreach($titles as $title => $link) { 
         ?>        
             <div class="card mb-2 mx-md-2" style="min-width: 12rem;">
                 <div class="card-body">
-                    <h5 class="card-title text-success"><?php echo $titles[$i];?></h5>
-                    <a href="<?php echo $links[$i];?>" class="btn btn-primary my-3">Acceder</a>
+                    <h5 class="card-title text-success"><?php echo $title;?></h5>
+                    <a href="<?php echo $link;?>" class="btn btn-primary my-3">Test</a>
                 </div>
             </div>
         <?php
