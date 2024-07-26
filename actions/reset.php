2a0c3ec37ae68d27d904f98eff06765e2d798031
Magdiel1990 <magdielmagdiel1@gmail.com>
2024-07-26 15:28:07 -0400

    Reset code done

--- actions/reset.php
+++ actions/reset.php
@@ -1,13 +1,24 @@
 <?php
-
-require "classes/Database.Class.php";
-$conn = DatabaseClassLoto::dbConnection();
+require_once ("pages/partials/head.php");
 
 //Reset the database
 if(isset($_GET["reset"])) {
+//Delete all numbers
+    $sql = "DELETE FROM numbers";
 
+    if($conn -> query($sql)) {
+        $_SESSION ["message"] = "Todos los números han sido eliminados";
+        $_SESSION ["message-alert"] = "success";
 
+        header("Location: " . root);
+        exit;      
+
+    } else {
+        $_SESSION ["message"] = "Error al eliminar números";
+        $_SESSION ["message-alert"] = "danger";
+
+        header("Location: " . root);
+        exit;           
+    }
 }
-
-
 ?>
