7cc5af637162b9bb1a98557568be5c8d899bcee9
Magdiel1990 <magdielmagdiel1@gmail.com>
2024-07-26 10:53:53 -0400

    minor path fix

--- pages/lototest.php
+++ pages/lototest.php
@@ -30,38 +30,9 @@
     require "methods/view_methods.php";
 
     require "partials/head.php";
-    require "partials/nav.php";
-
-    if(isset($_POST["numbers"])){
-        //Se ordenan los números
-        $numbers = sort($_POST["numbers"]);
-
-        //Se prueba el rango
-        $case = new RangeClass($numbers, $conn);
-        $test = $case -> testRange();
-
-        //Se excluyen las jugadas anteriores
-        $case = new PreviousPlaysOut ($test, $conn, $balls, $numbers);
-        $test = $case -> lastNumbersExceptions();
-
-        //Se prueba la suma total
-        $case = new TotalSum($test, $conn, $numbers);
-        $test = $case -> testTotalSum();
-
-        //Se prueba el promedio
-        $case = new Average($test, $conn, $numbers, $balls);
-        $test = $case -> averagePastGames();
-
-        //Se prueba de desviación estándar
-        $case = new StandardDeviation($test, $numbers, $balls, $conn);
-        $test = $case -> StdDev();     
-        
-        $case = new TotalProduct($test, $numbers, $balls, $conn);
-        $test = $case -> testTotalProduct ();
-    }    
-    $conn -> close();
+    
+    require "partials/nav.php";  
 ?>
-
 <main class="container p-4">
     <?php
         if(isset($_SESSION ["message"])){
@@ -87,8 +58,48 @@
                 <input class="btn btn-primary m-2" type="submit" value="Probar">
             </form>
         </div>
+        <?php
+            if(isset($_POST["numbers"])){
+                //Se ordenan los números
+                $numbers = sort($_POST["numbers"]);
+
+                //Se prueba el rango
+                $case = new RangeClass($numbers, $conn);
+                $test = $case -> testRange();
+
+                //Se excluyen las jugadas anteriores
+                $case = new PreviousPlaysOut ($test, $conn, $balls, $numbers);
+                $test = $case -> lastNumbersExceptions();
+
+                //Se prueba la suma total
+                $case = new TotalSum($test, $conn, $numbers);
+                $test = $case -> testTotalSum();
+
+                //Se prueba el promedio
+                $case = new Average($test, $conn, $numbers, $balls);
+                $test = $case -> averagePastGames();
+
+                //Se prueba de desviación estándar
+                $case = new StandardDeviation($test, $numbers, $balls, $conn);
+                $test = $case -> StdDev();     
+                
+                $case = new TotalProduct($test, $numbers, $balls, $conn);
+                $test = $case -> testTotalProduct ();
+
+                if($test){
+                    $html = '<div class="mt-3">';
+                    $html .= '<h4 class = "text-center text-success">La jugada es probable</h4>';
+                    $html .= '</div>';
+                } else {
+                    $html = '<div class="mt-3">';
+                    $html .= '<h4 class = "text-center text-danger">La jugada no es probable</h4>';
+                    $html .= '</div>';
+                }
+            }    
+        ?>
     </div>
 </main>
 <?php
+    $conn -> close();
     require ("partials/footer.php")
 ?>
