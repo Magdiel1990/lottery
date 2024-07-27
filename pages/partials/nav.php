8a3948e494f54ed31758753caef531321a6cc158
Magdiel1990 <magdielmagdiel1@gmail.com>
2024-07-27 10:36:41 -0400

    hiding the reset button

--- pages/partials/nav.php
+++ pages/partials/nav.php
@@ -11,10 +11,11 @@
       <li class="nav-item">
         <a class="nav-link" href="<?php echo root . 'estadistica'; ?>">Estadísticas</a>
       </li>
-
+<!--
       <li class="nav-item">
-        <a class="nav-link" id="reset" href="<?php echo root . 'reset?reset=true'; ?>">Reset</a>
+        <a class="nav-link" id="reset" href="<?php //echo root . 'reset?reset=true'; ?>">Reset</a>
       </li>
+-->
     </ul>
   </div>
 </nav>
