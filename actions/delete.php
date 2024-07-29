30a7fa372bbe5215a475b43490bbcf2e83f5a620
Magdiel1990 <magdielmagdiel1@gmail.com>
2024-07-28 22:36:05 -0400

    Eliminar numeros almacenados done

--- actions/delete.php
+++ actions/delete.php
@@ -3,7 +3,7 @@
 require("classes/Database.Class.php");
 $conn = DatabaseClassLoto::dbConnection();
 
-//Receive date
+//Eliminar números
 if(isset($_GET["date"])) {
     $date = $_GET["date"];
 
@@ -33,6 +33,40 @@ if(isset($_GET["date"])) {
         $_SESSION ["message-alert"] = "success";
 
         header('Location: ' . root . 'loto/agregar');
+        exit;
+    }
+}
+
+//Eliminar jugada almacenada para analizar
+if(isset($_GET['storedId'])){
+    $id = $_GET['storedId'];
+
+    $stmt = $conn -> prepare("SELECT id FROM stored WHERE id = ?;");
+    $stmt->bind_param("i", $storedId);    
+    $stmt -> execute();
+    $result = $stmt -> get_result(); 
+
+    if($result -> num_rows > 0) {
+        $resultDelete = $conn -> query("DELETE FROM stored WHERE id = '". $id ."';");
+
+        if($resultDelete) {
+            $_SESSION ["message"] = "Jugada eliminada correctamente";
+            $_SESSION ["message-alert"] = "success";
+
+            header('Location: ' . root . 'loto/test');
+            exit;
+        } else {
+            $_SESSION ["message"] = "Error al eliminar jugada";
+            $_SESSION ["message-alert"] = "danger";
+            
+            header('Location: ' . root . 'loto/test');
+            exit;
+        }  
+    } else {
+        $_SESSION ["message"] = "Esta jugada ya fue eliminada";
+        $_SESSION ["message-alert"] = "success";
+
+        header('Location: ' . root . 'loto/test');
         exit;
     }
 }
